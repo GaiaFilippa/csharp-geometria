14a03616948f991cbdea3ee4258a2a0f720df2f7
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-17 14:31:55 +0200

    Aggiungere i file di progetto.

---
+++ Geometria/Geometria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+        // ATTRIBUTI
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+        public int areaRettangolo;
+        public int perimetroRettangolo;
+     
+
+
+
+        // STATI
+
+
+
+        // COSTRUTTORE
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+
+        // METODI
+        public int CalcolaArea()
+        {
+            int areaRettangolo = baseRettangolo * altezzaRettangolo;
+            return areaRettangolo;
+        }
+
+
+        public int CalcolaPerimetro()
+        {
+            int perimetroRettangolo = baseRettangolo * 2 + altezzaRettangolo * 2;
+            return perimetroRettangolo;
+        }
+
+    }
+}
---
+++ Geometria/Program.cs
@@ -0,0 +1,14 @@
+﻿/*1. Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo.
+Aggiungere un opportuno costruttore con parametri.
+Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+2. Poi, nel vostro programma principale (Program.cs) chiedete all’utente di inserire, da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo.
+Dopo averlo istanziato, stampate a video il perimetro e l’area del rettangolo che avete costruito.
+3. Provate ad istanziare tanti rettangoli con nome diverso e attributi diversi (baseRettangolo e altezzaRettangolo cambiati)
+e provate a stampare le loro proprietà ad esempio con un metodo StampaRettangolo che vi stampi il vostro rettangolo ad esempio cosi:
+ —— Rettangolo ——
+base: 20 cm
+altezza: 10 cm
+Perimetro: 60 cm
+Area: 200 cm2
+ */
+
